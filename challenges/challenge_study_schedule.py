6f715997530185ae99c928c7ab2c972f0b0f9540
Cassio-Matsushita <superkoalabr@gmail.com>
2023-04-26 15:10:23 -0300

    requisito 1 completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int) or not all(
+        isinstance(t, tuple)
+        and len(t) == 2
+        and isinstance(t[0], int)
+        and isinstance(t[1], int)
+        for t in permanence_period
+    ):
+        return None
+
+    target_time_count = 0
+
+    for study_time, max_study_time in permanence_period:
+        if study_time <= target_time <= max_study_time:
+            target_time_count += 1
+
+    return target_time_count
