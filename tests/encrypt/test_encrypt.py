17b2da224a2d6e3d9b626808e35c1814807f58ff
Cassio-Matsushita <superkoalabr@gmail.com>
2023-04-26 15:59:35 -0300

    requisito 2 completo

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("cassio", 4) == "oi_ssac"
+    assert encrypt_message("cassio", 3) == "sac_ois"
+    assert encrypt_message("cassio", 9) == "oissac"
+    with pytest.raises(TypeError):
+        encrypt_message("retorna_erro", "1")
